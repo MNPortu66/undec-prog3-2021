79889eb389f5d0e1f0346da57f62e6ef75e8b794
Matias <mnportu@gmail.com>
2021-04-10 22:38:00 -0300

    Cuarta Entrega con Dependency Inversion

--- SOLIDPrinciples/src/AppBirdsTest.java
+++ SOLIDPrinciples/src/AppBirdsTest.java
@@ -2,9 +2,15 @@ public class AppBirdsTest {
     public static void main(String[] args) {
         Parrot bird1 =  new Parrot ();
         Penguin bird3 = new Penguin();
+        CreateBirdOracleDB createBirdOracleDB = new CreateBirdOracleDB();
+        CreateBIrdMySQLDB createBIrdMySQLDB = new CreateBIrdMySQLDB();
+
+        createBIrdMySQLDB.createBird(bird1);
+        createBirdOracleDB.createBird(bird3);
 
         System.out.println(bird1.toString());
         System.out.println(bird3.toString());
+
 
         bird1.eat();
         bird3.swims();
