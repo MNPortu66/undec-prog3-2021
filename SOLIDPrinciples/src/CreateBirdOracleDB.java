1316fdb2626162f225e51957223c391d48ed7536
Matias <mnportu@gmail.com>
2021-04-10 21:51:55 -0300

    Segunda Entrega con Single Responsibility

---
+++ SOLIDPrinciples/src/CreateBirdOracleDB.java
@@ -0,0 +1,10 @@
+public class CreateBirdOracleDB {
+    //Aplicando el primer principio estraemos el guardado del ave de la clase Bird y los hacemos aquí
+    Bird birdToSave = new Bird();
+
+    public void createBirdOracleDB(Bird birdToSave){
+        System.out.println("Se guarda el ave en base de dato oracle");
+
+    }
+
+}
